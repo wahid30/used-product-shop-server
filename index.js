03988de47f9c8890f52b025d567b711cbd765113
wahid30 <md.wahidulalamjp2@gmail.com>
2022-11-27 20:58:46 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000; //localhost:5000
 
+// middleware
 app.use(cors());
+app.use(express.json());
 
 // for categories
 const categories = require("./data/categories.json");
 // for all mobiles
 const mobiles = require("./data/mobiles.json");
+
+const uri =
+  "mongodb+srv://<username>:<password>@cluster0.fe54zrb.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
